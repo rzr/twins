15c6b726365ff137f796f52fd3d5fd8b866875fb
Philippe Coval <p.coval@samsung.com>
2019-05-21 09:58:48 +0200

    WIP: mod: controller.js (gh-pages)
    Change-Id: I338e29366282ce42f16b7394aa566855227d52ce
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 var app = {
   pause: false,
   property: 'torso',
-  url: 'https://webthing-iotjs-robot.glitch.me',
+  url: 'https://twins.glitch.me',
   suffix: '',
   useWs: false,
   wsUrl: 'ws://localhost:8888',
