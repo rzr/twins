e7b861f23f68b9cfc19c8a716eb078f795afe5eb
Philippe Coval <p.coval@samsung.com>
2019-07-26 19:14:44 +0200

    iotjs: Add launcher script for IoT.js on nuttx
    Change-Id: I8ad012a8d5e40bebe691b8b178a90aaa2e75c58e
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ extra/nuttx/index.js
@@ -0,0 +1,28 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// SPDX-License-Identifier: MPL-2.0
+
+/**
+ *
+ * Copyright 2019-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/#
+ */
+var console = require('console');
+// Disable logs here by editing to '!console.log'
+var log = console.log || function () {};
+var verbose = console.log || function () {};
+
+verbose(process);
+verbose(process.iotjs);
+verbose(process.iotjs.board);
+
+var app = require('twins');
+verbose(app);
+app.start();
+
+var loops = 0;
+setInterval(function() {
+  console.log('status: ' + loops++);
+}, 60 * 1000);
